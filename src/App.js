57338c657bfa30d7bf7667bce8db004db49f39fb
Fahad.Muhammad <fahad.muhammad@samipharma.com.pk>
2022-06-11 13:04:07 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyModal from './components/MyModal';
+import $ from 'jquery';
+import { useEffect, useState } from 'react';
 function App() {
+  const searchText=(event)=>{
+    var text=event.target.value;
+    console.log(text);
+  }
+
+  const windowClose=(isRefresh)=>{
+    console.log("isRefresh"+isRefresh);
+  }
+  const fetchData=()=>{
+    
+  }
+  useEffect(()=>{
+
+  },[]);
+  const [todo,setTodo] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header container'>
+        <div className="row" style={{marginTop:"10px"}}>
+          <div className="col-md-8 mx-auto" >
+          <input type='text' placeholder='Search By title' className='form-control' onChange={searchText} />
+          </div>
+        </div>
+        <div className="row" style={{marginTop:"10px"}}>
+          <div className='col-md-6 mx-auto'>
+              <button type='submit' className='btn btn-success' data-toggle="modal" data-target="#modelId">Add Todo</button>
+          </div>
+        </div>
+        <MyModal data={todo} window={windowClose}/>
+      </div>
     </div>
   );
 }
